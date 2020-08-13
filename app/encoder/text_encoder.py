94acd026a22b30276b2f30df26b67aa8e5def182
mishraprafful <mishraprafful@gmail.com>
2020-08-13 21:37:50 +0530

    chnaged device name

--- app/encoder/text_encoder.py
+++ app/encoder/text_encoder.py
@@ -5,13 +5,13 @@ from transformers import BertTokenizer
 from network.text_model import BertForFeatureExtraction
 
 # configuring device
-device = 'cuda' if cuda.is_available() else 'cpu'
+machine_device = 'cuda' if cuda.is_available() else 'cpu'
 
 # loading the model and tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 feature_extractor = BertForFeatureExtraction()
 feature_extractor.freeze_bert_encoder()
-feature_extractor.to(torch.device(device))
+feature_extractor.to(device(machine_device))
 
 
 def vectorise(batch_text):
@@ -25,8 +25,7 @@ def vectorise(batch_text):
     input_ids = tokenizer.batch_encode_plus(
         batch_text_or_text_pairs=batch_text, add_special_tokens=True, pad_to_max_length=True, return_tensors="pt")['input_ids']
 
-    input_ids.to(torch.device(device))
-
-    result = feature_extractor(input_object).cpu().tolist()
+    input_ids.to(device(machine_device))
+    result = feature_extractor(input_ids)
 
     return result
