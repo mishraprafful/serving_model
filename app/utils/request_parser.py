538d56603482c2faf72e8e960f229f58d4904631
mishraprafful <mishraprafful@gmail.com>
2020-08-13 23:49:34 +0530

    added text and image_url validation

--- app/utils/request_parser.py
+++ app/utils/request_parser.py
@@ -1,3 +1,9 @@
+import requests
+import validators
+from io import BytesIO
+from PIL import Image, UnidentifiedImageError
+
+
 def parse_request(request):
     """ Parse and validate Vectorizer Request
     Arguments:
@@ -9,8 +15,23 @@ def parse_request(request):
     error = None
 
     # validating the request json to be a list and to have both the required keys for vectorisation
-    if isinstance(request.json, list) and all([list(element.keys()) == ['text', 'image'] for element in request.json]):
-        pass
+    if isinstance(request.json, list) and \
+            all([list(element.keys()) == ['text', 'image'] for element in request.json]):
+        for entity in request.json:
+
+            # validating url and text
+            if all([isinstance(element, str) for element in entity.values()]) and \
+                    (validators.url(entity['image']) or entity['image'] == ""):
+
+                # validating image url
+                if entity['image']:
+                    try:
+                        response = requests.get(entity['image'])
+                        Image.open(BytesIO(response.content))
+                    except UnidentifiedImageError as e:
+                        error = {'error': 'Invalid Request'}
+            else:
+                error = {'error': 'Invalid Request'}
     else:
         error = {'error': 'Invalid Request'}
     return error, request
