1e9a41aa9608fe6cc27813207f0f53679f19a4c6
mishraprafful <mishraprafful@gmail.com>
2020-08-13 17:52:38 +0530

    added text encoder

---
+++ app/encoder/text_encoder.py
@@ -0,0 +1,32 @@
+import os
+from torch import cuda, device
+
+from transformers import BertTokenizer
+from network.text_model import BertForFeatureExtraction
+
+# configuring device
+device = 'cuda' if cuda.is_available() else 'cpu'
+
+# loading the model and tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+feature_extractor = BertForFeatureExtraction()
+feature_extractor.freeze_bert_encoder()
+feature_extractor.to(torch.device(device))
+
+
+def vectorise(batch_text):
+    """ Vectorise Text
+    Arguments:
+       [list] -- [list of str]
+    Returns:
+        [list] -- [list of tensors]
+    """
+
+    input_ids = tokenizer.batch_encode_plus(
+        batch_text_or_text_pairs=batch_text, add_special_tokens=True, pad_to_max_length=True, return_tensors="pt")['input_ids']
+
+    input_ids.to(torch.device(device))
+
+    result = feature_extractor(input_object).cpu().tolist()
+
+    return result
